f4a7a37b1cc889e068c084a300d8e6e9ba18075f
Shawn Stewart <s.stewart0220@gmail.com>
2019-01-09 01:50:09 -0500

    Templates are rendering dynamic data passed in from views

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Shawn',
+        'title': 'Exploring Django!',
+        'content': "Django is really neat!",
+        'date_posted': 'January 9, 2019'
+    },
+    {
+        'author': 'Mallorie',
+        'title': 'My First Blog Post',
+        'content': "Here is the blog content.",
+        'date_posted': 'January 9, 2019'
+    }
+]
+
 
 def test(request):
     return HttpResponse('<p>Test route working!</p>')
 
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
